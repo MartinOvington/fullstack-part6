94be6ee3a9a86467e9a9ed24937fb30746efd45e
Martin Ovington <MartinOvington@msn.com>
2022-03-17 09:29:03 +0000

    ex6.7 changed component name to AnecdoteForm

--- anecdotes-redux/src/App.js
+++ anecdotes-redux/src/App.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { incVote } from './reducers/anecdoteReducer'
-import NewAnecdote from './components/NewAnecdote'
+import NewAnecdote from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
