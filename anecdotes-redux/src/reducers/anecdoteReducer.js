479d43f448b94f2833205fe01524d489501ec716
Martin Ovington <MartinOvington@msn.com>
2022-03-17 09:22:10 +0000

    ex6.4 implemented adding new anecdotes

--- anecdotes-redux/src/reducers/anecdoteReducer.js
+++ anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -33,8 +33,17 @@ const reducer = (state = initialState, action) => {
       }
       return state.map(anecdote =>
         anecdote.id !== id ? anecdote : changedAnecdote)
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
     default:
       return state
+  }
+}
+
+export const createAnecdote = (anecdote) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: asObject(anecdote)
   }
 }
 
