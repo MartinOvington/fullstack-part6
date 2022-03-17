a4fd903ba71c9797d700715ff68df55e5d6f00a6
Martin Ovington <MartinOvington@msn.com>
2022-03-17 09:53:18 +0000

    ex6.8, AnecdoteList moved into separate component

---
+++ anecdotes-redux/src/components/AnecdoteList.js
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { incVote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleVote }) => {
+  return (
+    <div>
+      {anecdote.content}
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleVote}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  return (
+    <div>
+      {anecdotes.sort((a, b) => b.votes - a.votes)
+        .map(anecdote =>
+          <Anecdote
+            key={anecdote.id}
+            anecdote={anecdote}
+            handleVote={() =>
+              dispatch(incVote(anecdote.id))
+            }
+          />
+        )}
+    </div>
+  )
+}
+
+export default AnecdoteList
+
