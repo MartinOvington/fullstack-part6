82508466274c20fa2463ac75884761512501e13a
Martin Ovington <MartinOvington@msn.com>
2022-03-18 08:13:43 +0000

    ex6.8 changed NewAnecdote to AnecdoteForm to fit exercise description. Changed order of form and list.

--- anecdotes-redux/src/App.js
+++ anecdotes-redux/src/App.js
@@ -1,12 +1,12 @@
-import NewAnecdote from './components/AnecdoteForm'
+import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <AnecdoteForm />
       <AnecdoteList />
-      <NewAnecdote />
     </div>
   )
 }
