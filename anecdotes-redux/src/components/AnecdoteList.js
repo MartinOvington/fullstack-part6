394f59e5aba73b8dc7d8bbcf28e1f0879e4c451f
Martin Ovington <MartinOvington@msn.com>
2022-03-21 09:50:42 +0000

    ex6.12 added anecdote filter

--- anecdotes-redux/src/components/AnecdoteList.js
+++ anecdotes-redux/src/components/AnecdoteList.js
@@ -17,9 +17,12 @@ const Anecdote = ({ anecdote, handleVote }) => {
 const AnecdoteList = () => {
   const dispatch = useDispatch()
   const anecdotes = useSelector(({ anecdotes }) => anecdotes)
+  const filter = useSelector(({ filter }) => filter)
   return (
     <div>
-      {[...anecdotes].sort((a, b) => b.votes - a.votes)
+      {[...anecdotes]
+        .sort((a, b) => b.votes - a.votes)
+        .filter((anecdote) => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
         .map(anecdote =>
           <Anecdote
             key={anecdote.id}
