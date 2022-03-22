ee54f44f48d43c6c9cc2c2044c06be11add2eead
Martin Ovington <MartinOvington@msn.com>
2022-03-22 09:39:34 +0000

    ex6.15, moved store up one level, store initialize is done using asynchronous action creators and the Redux Thunk library

--- anecdotes-redux/src/App.js
+++ anecdotes-redux/src/App.js
@@ -3,15 +3,13 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/FilterAnecdotes'
-import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
-import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    anecdoteService
-      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+    dispatch(initializeAnecdotes())
   }, [dispatch])
 
   return (
